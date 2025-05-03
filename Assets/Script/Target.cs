d8a3781faa2a571805e0324d8d1a0f1496196840
AhmadFachri <49482566+Nakashihc@users.noreply.github.com>
2025-05-03 14:38:02 +0700

    Spawn Object

--- Assets/Script/Target.cs
+++ Assets/Script/Target.cs
@@ -9,9 +9,41 @@ public class Target : MonoBehaviour
     public bool isTarget;
     private ScoreManager scoreManager;
 
+    public float moveSpeed = 3f;
+    private List<Transform> waypoints = new List<Transform>();
+    private int currentWaypointIndex = 0;
     void Start()
     {
         scoreManager = FindAnyObjectByType<ScoreManager>();
+    }
+
+    private void Update()
+    {
+        if (waypoints == null || waypoints.Count == 0)
+            return;
+
+        MoveToWaypoint();
+    }
+
+    public void SetWaypoints(List<Transform> waypointList)
+    {
+        waypoints = waypointList;
+        currentWaypointIndex = 0;
+    }
+
+    private void MoveToWaypoint()
+    {
+        Transform targetWaypoint = waypoints[currentWaypointIndex];
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Count)
+            {
+                currentWaypointIndex = 0; // Loop kembali ke waypoint pertama
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -26,4 +58,6 @@ public class Target : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+
 }
